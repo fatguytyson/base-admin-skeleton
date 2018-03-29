a0491ba0559214ef57743d6eb6ed72e991092af9
Tyson Green <tyson@fatguyconsulting.com>
2018-03-28 22:23:14 -0700

    HWI OAUTH Test

---
+++ src/AppBundle/Util/UserManagerInterface.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+namespace AppBundle\Util;
+
+use AppBundle\Entity\User;
+
+interface UserManagerInterface
+{
+	/**
+	 * Returns an empty user instance
+	 *
+	 * @return User
+	 */
+    public function createUser();
+
+	/**
+	 * Finds a user by email
+	 *
+	 * @param string $email
+	 *
+	 * @return User
+	 */
+    public function findUserByEmail($email);
+
+	/**
+	 * Finds a user by username
+	 *
+	 * @param string $username
+	 *
+	 * @return User
+	 */
+    public function findUserByUsername($username);
+
+	/**
+	 * Finds a user either by email, or username
+	 *
+	 * @param string $usernameOrEmail
+	 *
+	 * @return User
+	 */
+    public function findUserByUsernameOrEmail($usernameOrEmail);
+
+	/**
+	 * Finds a user either by confirmation token
+	 *
+	 * @param string $token
+	 *
+	 * @return User
+	 */
+    public function findUserByConfirmationToken($token);
+
+	/**
+	 * @param User $user
+	 *
+	 * @return void
+	 */
+    public function updateCanonicalFields(User $user);
+
+	/**
+	 * @param User $user
+	 *
+	 * @return void
+	 */
+    public function updatePassword(User $user);
+
+	/**
+	 * @param User $user
+	 *
+	 * @return void
+	 */
+    public function deleteUser(User $user);
+
+	/**
+	 * @return string
+	 */
+    public function getClass();
+
+	/**
+	 * @param array $criteria
+	 *
+	 * @return User|null
+	 */
+    public function findUserBy(array $criteria);
+
+	/**
+	 * @return array
+	 */
+    public function findUsers();
+
+	/**
+	 * @param User $user
+	 *
+	 * @return void
+	 */
+    public function reloadUser(User $user);
+
+	/**
+	 * Updates a user.
+	 *
+	 * @param User      $user
+	 * @param Boolean   $andFlush Whether to flush the changes (default true)
+	 */
+    public function updateUser(User $user, $andFlush = true);
+}
