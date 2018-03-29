834541ee30e99b341cd86c3234b716286002302b
Tyson Green <tyson@fatguyconsulting.com>
2018-03-29 01:41:44 -0700

    HWI OAUTH Test

--- src/AppBundle/Util/UserManager.php
+++ src/AppBundle/Util/UserManager.php
@@ -194,7 +194,6 @@ class UserManager implements UserManagerInterface
      */
     public function findUserBy(array $criteria)
     {
-    	dump($criteria);
         return $this->repository->findOneBy($criteria);
     }
 
